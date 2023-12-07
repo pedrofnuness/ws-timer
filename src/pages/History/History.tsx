333515d7086417c585f7025395825070085febb1
Pedro Nunes <pedronunes.dev@gmail.com>
2023-12-07 18:30:14 -0300

    feat: format startDate at history screen

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import * as Styled from './styles'
 import { CyclesContext } from '../../contexts/CyclesContext';
+import { formatDistanceToNow } from 'date-fns';
 
 export function History() {
   const { cycles } = useContext(CyclesContext);
@@ -24,7 +25,9 @@ export function History() {
               <tr key={cycle.id}>
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutes</td>
-                <td>{cycle.startDate.toISOString()}</td>
+                <td>{formatDistanceToNow(cycle.startDate, {
+                  addSuffix: true
+                })}</td>
                 <td>
                   {cycle.finishedDate && (
                     <Styled.Status statusColor="green">Completed</Styled.Status>
