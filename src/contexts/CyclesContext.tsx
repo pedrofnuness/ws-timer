53bd75fd4373eed85efc48dd8d303899b907f42c
Pedro Nunes <pedronunes.dev@gmail.com>
2023-12-11 15:56:48 -0300

    refactor: moved action types to ENUM that can be reused along the codebase

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState, ReactNode, useReducer } from 'react';
-import { Cycle, cyclesReducer } from '../reducers/cycles';
+import { ActionTypes, Cycle, cyclesReducer } from '../reducers/cycles';
 
 interface CreateCycleData {
   task: string;
@@ -41,7 +41,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
 
   function markCurrentCycleAsFinished() {
     dispatch({
-      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      type: ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED,
       payload: {
         activeCycleId,
       }
@@ -56,7 +56,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
       startDate: new Date()
     };
     dispatch({
-      type: 'ADD_NEW_CYCLE',
+      type: ActionTypes.ADD_NEW_CYCLE,
       payload: {
         newCycle,
       }
@@ -67,7 +67,7 @@ export function CyclesContextProvider({ children }: CyclesContextProviderProps) 
 
   function interruptCurrentCycle() {
     dispatch({
-      type: 'INTERRUPT_CURRENT_CYCLE',
+      type: ActionTypes.INTERRUPT_CURRENT_CYCLE,
       payload: {
         activeCycleId,
       }
--- src/reducers/cycles.ts
+++ src/reducers/cycles.ts
@@ -1,8 +1,3 @@
-interface CyclesState {
-  cycles: Cycle[]
-  activeCycleId: string | null
-}
-
 export interface Cycle {
   id: string;
   task: string;
@@ -12,15 +7,26 @@ export interface Cycle {
   finishedDate?: Date,
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
+export enum ActionTypes {
+  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
+  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
+  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
+}
+
 export function cyclesReducer(state: CyclesState, action: any) {
   switch (action.type) {
-    case 'ADD_NEW_CYCLE':
+    case ActionTypes.ADD_NEW_CYCLE:
       return {
         ...state,
         cycles: [...state.cycles, action.payload.newCycle],
         activeCycleId: action.payload.newCycle.id
       }
-    case 'INTERRUPT_CURRENT_CYCLE':
+    case ActionTypes.INTERRUPT_CURRENT_CYCLE:
       return {
         ...state,
         cycles: state.cycles.map(cycle => {
@@ -32,7 +38,7 @@ export function cyclesReducer(state: CyclesState, action: any) {
         }),
         activeCycleId: null
       }
-    case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+    case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED:
       return {
         ...state,
         cycles: state.cycles.map(cycle => {
